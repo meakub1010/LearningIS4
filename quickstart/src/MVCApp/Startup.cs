78a308a70887a2c8940b71f5cca869c660140517
Muhammad Eakub <meakub86@gmail.com>
2020-02-28 17:40:16 -0500

    adding oidc in MVC app

--- quickstart/src/MVCApp/Startup.cs
+++ quickstart/src/MVCApp/Startup.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace MVCApp
 {
@@ -33,6 +29,24 @@ namespace MVCApp
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+               .AddCookie("Cookies")
+               .AddOpenIdConnect("oidc", options =>
+               {
+                   options.Authority = "http://localhost:5000";
+                   options.RequireHttpsMetadata = false;
+
+                   options.ClientId = "mvc.client";
+                   options.SaveTokens = true;
+               });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,9 +62,16 @@ namespace MVCApp
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            
+
+            
 
             app.UseMvc(routes =>
             {
