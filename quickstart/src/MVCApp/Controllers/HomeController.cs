82b6c3274e960cc42858a082520071b2cdf83de7
Muhammad Eakub <meakub86@gmail.com>
2020-03-04 17:37:37 -0500

    calling api from mvc app

--- quickstart/src/MVCApp/Controllers/HomeController.cs
+++ quickstart/src/MVCApp/Controllers/HomeController.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MVCApp.Models;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace MVCApp.Controllers
 {
@@ -26,6 +31,20 @@ namespace MVCApp.Controllers
             ViewData["Message"] = "Your contact page.";
 
             return View();
+        }
+
+        [Route("callapi")]
+        [Authorize(Policy = "MinimumAge")]
+        public async Task<IActionResult> CallApi()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var content = await client.GetStringAsync("http://localhost:5001/identity/claims");
+
+            ViewBag.Json = JArray.Parse(content).ToString();
+            return View("CallApi");
         }
 
         public IActionResult Privacy()
